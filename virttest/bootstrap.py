d1a80ec9a3e022e2aafc0e905b3a361c640cf2cd
Lucas Meneghel Rodrigues <lmr@redhat.com>
2015-06-30 13:12:04 -0300

    Add spice backend
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- virttest/bootstrap.py
+++ virttest/bootstrap.py
@@ -17,6 +17,8 @@ basic_program_requirements = ['7za', 'tcpdump', 'nc', 'ip', 'arping']
 
 recommended_programs = {'qemu': [('qemu-kvm', 'kvm'), ('qemu-img',),
                                  ('qemu-io',)],
+                        'spice': [('qemu-kvm', 'kvm'), ('qemu-img',),
+                                  ('qemu-io',)],
                         'libvirt': [('virsh',), ('virt-install',),
                                     ('fakeroot',), ('semanage',),
                                     ('getfattr',), ('restorecon',)],
@@ -26,6 +28,7 @@ recommended_programs = {'qemu': [('qemu-kvm', 'kvm'), ('qemu-img',),
                         'libguestfs': [('perl',)]}
 
 mandatory_programs = {'qemu': basic_program_requirements + ['gcc'],
+                      'spice': basic_program_requirements + ['gcc'],
                       'libvirt': basic_program_requirements,
                       'openvswitch': basic_program_requirements,
                       'lvsb': ['virt-sandbox', 'virt-sandbox-service', 'virsh'],
@@ -33,6 +36,7 @@ mandatory_programs = {'qemu': basic_program_requirements + ['gcc'],
                       'libguestfs': basic_program_requirements}
 
 mandatory_headers = {'qemu': ['Python.h', 'types.h', 'socket.h', 'unistd.h'],
+                     'spice': [],
                      'libvirt': [],
                      'openvswitch': [],
                      'v2v': [],
@@ -40,6 +44,7 @@ mandatory_headers = {'qemu': ['Python.h', 'types.h', 'socket.h', 'unistd.h'],
                      'libguestfs': []}
 
 first_subtest = {'qemu': ['unattended_install', 'steps'],
+                 'spice': ['unattended_install', 'steps'],
                  'libvirt': ['unattended_install'],
                  'openvswitch': ['unattended_install'],
                  'v2v': ['unattended_install'],
@@ -47,6 +52,7 @@ first_subtest = {'qemu': ['unattended_install', 'steps'],
                  'lvsb': []}
 
 last_subtest = {'qemu': ['shutdown'],
+                'spice': ['shutdown'],
                 'libvirt': ['shutdown', 'remove_guest'],
                 'openvswitch': ['shutdown'],
                 'v2v': ['shutdown'],
